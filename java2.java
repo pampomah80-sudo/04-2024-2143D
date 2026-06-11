d01cbe9081dc52a7cc72186b305da0240fafb38e
pampomah80-sudo <pampomah80@gmail.com>
2026-06-10 13:29:49 -1200

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,21 @@
+public class java2 {
+    public static void main(String[] args){
+
+        System.out.println(
+      "X                       X");
+       System.out.println(
+      "X                       X");
+       System.out.println(
+      "X      XXXXXXXXXX       X");
+       System.out.println(
+      "XXXX   X        X   XXXXX");
+       System.out.println(
+      "X  X  X          X  X  X");
+       System.out.println(
+      "X  X  X          X  X  X");
+
+
+
+
+    }
+}
